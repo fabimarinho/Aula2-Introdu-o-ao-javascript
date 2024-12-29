cf277ad52f569e34856df7bf426fff8a05dfffab
fabi marinho <fabiane29201@gmail.com>
2024-12-29 12:16:32 -0300

    atualização do código

--- script.js
+++ script.js
@@ -1,17 +1,28 @@
 function adicionarTarefa() {
   // Recebe valor do input do usuário
-  let inputTarefa = document.getElementById("inputTarefa");
-  let tarefa = inputTarefa.value;
+  const inputTarefa = document.getElementById("inputTarefa");
+  let tarefa = inputTarefa.value.trim();
 
-  //   Cria novo item (li) e insere na (lista ul)
-  let listaTarefas = document.getElementById("listaTarefas");
-  let novaTarefa = document.createElement("li");
-  novaTarefa.textContent = tarefa;
-  listaTarefas.appendChild(novaTarefa);
+  const mensagem = document.getElementById("mensagem");
 
-  //   Mensagem de tarefa adicionada com sucesso
-  let mensagem = "Tarefa adicionada com sucesso!";
-  document.getElementById("mensagem").textContent = mensagem;
+  //  Se o valor do input for vazio então mostra uma mensagem de erro para o usuário
+  if (tarefa == "") {
+    // mostre uma mensagem de erro
+    let mensagemErro = "Por favor, insira uma tarefa!";
+    mensagem.textContent = mensagemErro;
+    mensagem.style.color = "#A34743";
+  } else {
+    //   Mensagem de tarefa adicionada com sucesso
+    let mensagemSucesso = "Tarefa adicionada com sucesso!";
+    mensagem.textContent = mensagemSucesso;
+    mensagem.style.color = "#28A745";
+
+    //   Cria novo item (li) e insere na (lista ul)
+    const listaTarefas = document.getElementById("listaTarefas");
+    let novaTarefa = document.createElement("li");
+    novaTarefa.textContent = tarefa;
+    listaTarefas.appendChild(novaTarefa);
+  }
 
   //   limpa o input do usuário
   inputTarefa.value = "";
